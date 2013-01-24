cae6571e36be690fd8c94f59b8f99ea1527aec07
aron <aroni11@ru.is>
2013-01-24 14:46:01 +0000

    First commit, framework for assignment

---
+++ src/UserAgent.java
@@ -0,0 +1,20 @@
+import java.util.Collection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: aron
+ * Date: 1/24/13
+ * Time: 1:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class UserAgent implements Agent {
+    @Override
+    public void init(Collection<String> percepts) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public String nextAction(Collection<String> percepts) {
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
